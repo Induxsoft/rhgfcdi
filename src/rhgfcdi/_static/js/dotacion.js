9031c748a6df95435896f29aa7f428c4f1815a5d
Francisco Ávila <50455756+7oni3c@users.noreply.github.com>
2024-04-29 17:16:00 -0600

    update: Gestión de dotación de insumos a empleados
    Mostrar almacenes autorizados del puesto al que pertenece el empleado.
    Generar salidas de inventario.
    Validación del excedente.

--- src/rhgfcdi/_static/js/dotacion.js
+++ src/rhgfcdi/_static/js/dotacion.js
@@ -1,15 +1,22 @@
 var dotacion =
 {
+    formId:"", form:null,
     url_obtener_almacenes:"",
+    url_validar_excedente:"",
 
     init()
     {
         const ik_empleado = document.getElementById("ik_empleado");
         const ik_insumo = document.getElementById("ik_insumo");
         const txt_unidad = document.getElementById("txt_unidad");
+        const btn_submit = document.getElementById("btn_submit");
+        this.form = document.getElementById(this.formId);
 
-        ik_empleado.addEventListener("change", (data) => { this.fillSelect("sel_almacen","ialmacen","dalmacen",this.url_obtener_almacenes,{iempleado:data.sys_pk}) });
-        ik_insumo.addEventListener("change", (data) => { txt_unidad.value = (data.unidad ?? "").toUpperCase() });
+        if (btn_submit) btn_submit.addEventListener("click", () => this.submit());
+        ik_empleado.change_event = (data) => { this.fillSelect("sel_almacen","ialmacen","dalmacen",this.url_obtener_almacenes,{iempleado:data.sys_pk}) }
+        ik_insumo.change_event = (data) => { txt_unidad.value = (data.unidad ?? "").toUpperCase() }
+
+        ik_empleado.setValue(ik_empleado.getValue());
     },
 
     fillSelect(id, kf, vf, url, params={})
@@ -37,5 +44,27 @@ var dotacion =
             });
         })
         .catch(error => console.error(error));
-    }
+    },
+
+    submit()
+    {
+        let endpoint = this.url_validar_excedente;
+        let fd = new FormData(this.form);
+
+        const onSuccess = (data) =>
+        {
+            if (data.message)
+            {
+                alert(data.message);
+                return;
+            }
+
+            if (data.excedente && !confirm("El empleado esta excediendo el limite de insumos establecido. ¿Desea continuar?")) return;
+            this.form.submit();
+        }
+
+        const onFailure = (error) => { alert(error.message ?? JSON.stringify(error)) }
+
+        InduxsoftCrudlModel.InvokeService(endpoint,fd,onSuccess,onFailure,"POST",false,true,"",true);
+    },
 }
